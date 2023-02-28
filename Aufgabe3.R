e6aff602759a0236c13acc6b0c3fd1accada9306
mathfre <122696830+mathfre@users.noreply.github.com>
2023-02-28 18:54:13 +0100

    Update Aufgabe3.R

--- Aufgabe3.R
+++ Aufgabe3.R
@@ -86,23 +86,6 @@ funktionf<- function(data, var1, var2, var3   , var4 = NULL) {
 }
 
 
-#Hilfsfunktion zur Berechnung von cramers_V()
-chiquadrat_rechner <- function(x,y){ #Chi Quadrat Formel
-  if(is.null(x) || is.null(y)){
-    stop("Error: Einer der beiden Variablen ist leer")
-  } # Abbruchkriterium
-  chi <- 0
-  dummy <- table(x,y) #Kreuztabelle wird erzeugt zwischen x und y
-  rs <- rowSums(dummy)  #Summen der einzelnen Zeilen der Tabelle
-  cs <- colSums(dummy)  #Summen der einzelnen Spalten der Tabelle
-  
-  for (i in 1:length(rs)) { #Allgemeine Rechnenformel für Chi Quadrat 
-    for(j in 1:length(cs)){
-      chi <- chi + ((rs[i] * cs[j])/length(x) - dummy[i,j])^2/((rs[i] * cs[j])/length(x))
-    }
-  }
-  return(chi) #Output
-}
 
 
                  
