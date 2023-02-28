4c9fa9f0a1e72df5ab46f56f93beda139830acb7
Naim Yilmaz <125359896+naimymz@users.noreply.github.com>
2023-02-28 09:18:16 +0100

    Rechtschreibfehler in den Kommentaren

--- Cramers V und biseriale Korr( c und d).R
+++ Cramers V und biseriale Korr( c und d).R
@@ -1,12 +1,12 @@
 #Funktion zur Berechnung für den Zusammenhang zweier bivariater kategorieller Variablen
-cramers_V <- function(x,y){  #Cramers V als Index für Korrelatin
+cramers_V <- function(x,y){  #Cramers V als Index für Korrelation
   if(is.null(x) || is.null(y)){
     stop("Error: Einer der beiden Variablen ist leer")
   } # Abbruchkriterium
-  anzSpalten <- length(colSums(table(alter,studienfach))) #Anzahl an Spalten in der
+  anzSpalten <- length(colSums(table(alter,studienfach))) # Anzahl an Spalten in der
   anzZeilen <- length(rowSums(table(alter,studienfach)))  # Tabelle
 #Allgemeine Formel von Cramers v: 
-#Chi_squared/ (Anzahl Beobachtungen * Minimum Zeilen und Spalten in Tabellendarstellung - 1)
+#Chi_squared/ (Anzahl Beobachtungen * Minimum(Zeilen,Spalten) - 1)
     v <- sqrt(chiquadrat_rechner(x,y)/(length(x) * # Allgemeine Formel von Cramers V
               (min(anzSpalten,anzZeilen) - 1)))
   return(v) #Output zwischen 0 und 1
@@ -21,10 +21,10 @@ r_pbis <- function(x,y){ #Punkt biseriale Korrelation, nimmt Werte zwischen -1 u
     stop("Error: x muss die dichotome Variable sein und aus 0 und 1 bestehen!")
   } #Ausnahmefall.
   yx_0 <- mean(y[x == 0]) #Mittelwert von y wo x = 0 ist
-  yx_1 <- mean(y[x == 1]) #Mittelwert von y wo y = 1 ist 
+  yx_1 <- mean(y[x == 1]) #Mittelwert von y wo x = 1 ist 
   # Allgemeine Formel für die Rechnung von Punktbiserialen Korrelation
   r <- (yx_1 - yx_0)/sd(y) * sqrt(length(x[x == 0])*length(x[x == 0]))/length(x)
   return(r) #Output zwischen -1 und 1, sonst Fehler.
-  # -1 starker neg. Zusammenhang, 0 keinen, 1 starker pos. Zusammenhanger 
+  # -1 starker neg. Zusammenhang, 0 keinen, 1 starker pos. Zusammenhang
 }
        
